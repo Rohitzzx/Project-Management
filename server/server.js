bd0d783478e1a5dfa13220dcb0d7588b756ed450
Rohit Birajdar <rohitbirajdar717@gmail.com>
2025-11-05 22:49:00 +0530

    commit

--- server/server.js
+++ server/server.js
@@ -1,9 +1,9 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import cors from 'cors';
-import { clerkMiddleware } from '@clerk/express'
-import { inngest, functions } from './inngest/index.js';
-import { serve } from 'inngest/express';
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import { clerkMiddleware } from "@clerk/express";
+import { inngest, functions } from "./inngest/index.js";
+import { serve } from "inngest/express";
 
 const app = express();
 
@@ -11,9 +11,8 @@ app.use(express.json());
 app.use(cors());
 app.use(clerkMiddleware());
 
-
-app.get('/', (req, res) => {
-    res.send('Server is running');
+app.get("/", (req, res) => {
+  res.send("Server is running");
 });
 
 app.use("/api/inngest", serve({ client: inngest, functions }));
@@ -21,5 +20,5 @@ app.use("/api/inngest", serve({ client: inngest, functions }));
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
